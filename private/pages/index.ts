313316f10110e77809193bdde9fb466e1cf777db
marekkobida <marek.kobida@gmail.com>
2020-01-10 15:26:06 +0100

    updated pages

--- private/pages/index.ts
+++ private/pages/index.ts
@@ -1,7 +1,23 @@
+import Container from '@redred/pages/private/Container';
+
 import Design from './Design';
 
 const pages = [
   new Design(),
 ];
 
-export default pages;
+const container = new Container(-1, 'design', pages);
+
+if (typeof window !== 'undefined') {
+  for (let i = 0; i < container.pages.length; i += 1) {
+    const page = container.pages[i];
+
+    try {
+      page.toDOM();
+    } catch (error) {
+
+    }
+  }
+}
+
+export default container;
