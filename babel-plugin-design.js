93fedd4f1f694a09b45ff7549260157bb76b662c
marekkobida <marek.kobida@gmail.com>
2020-02-19 09:38:00 +0100

    updated compiler

---
+++ babel-plugin-design.js
@@ -0,0 +1,41 @@
+module.exports = ({ types: t, }) => {
+  const cloneNode = t.cloneNode || t.cloneDeep;
+
+  return {
+    visitor: {
+      Program: {
+        enter (path, state) {
+          state.createClassNameIdentifier = path.scope.generateUidIdentifier('createClassName');
+        },
+        exit (path, state) {
+          if (state.$) {
+            path.node.body.unshift(t.importDeclaration([ t.importSpecifier(state.createClassNameIdentifier, t.identifier('createClassName')), ], t.stringLiteral('@redred/design')));
+          }
+        },
+      },
+      JSXAttribute (path, state) {
+        if (path.node.name.name !== 'className') {
+          return;
+        }
+
+        const value = path.get('value');
+
+        if (value.isJSXExpressionContainer()) {
+          const expression = value.get('expression');
+
+          if (expression.isArrayExpression()) {
+            expression.replaceWith(t.callExpression(cloneNode(state.createClassNameIdentifier), expression.get('elements').map((e) => cloneNode(e.node))));
+
+            state.$ = true;
+          }
+        }
+
+        if (value.isStringLiteral()) {
+          value.replaceWith(t.JSXExpressionContainer(t.callExpression(cloneNode(state.createClassNameIdentifier), [ t.stringLiteral(value.node.value), ])));
+
+          state.$ = true;
+        }
+      },
+    },
+  };
+};
