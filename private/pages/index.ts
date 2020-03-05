e1b7c9e3b6e0c843a7d4883c6d69b430cb53a223
marekkobida <marek.kobida@gmail.com>
2020-03-05 12:18:34 +0100

    updated compiler

--- private/pages/index.ts
+++ private/pages/index.ts
@@ -4,12 +4,6 @@ import Test from './Test';
 
 const container = new Container([ new Test(), ]);
 
-if (typeof window !== 'undefined') {
-  for (let i = 0; i < container.pages.length; i += 1) {
-    const page = container.pages[i];
-
-    page.toDOM();
-  }
-}
+container.toDOM();
 
 export default container;
