0032948c9289e5957617e0d35d5a86a9f0838817
Marek Kobida <marek.kobida@gmail.com>
2020-05-31 17:27:16 +0200

    deleted design page

--- private/pages/index.ts
+++
@@ -1,8 +0,0 @@
-import Container from '@redredsk/pages/private/Container';
-import Test from './Test';
-
-const container = new Container(-1, 'design', [ new Test(), ]);
-
-container.toDOM();
-
-export default container;
