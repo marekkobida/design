3f285b1aa1117ad77f0aa121afd2270834c8daa1
Marek Kobida <marek.kobida@gmail.com>
2020-06-25 09:07:18 +0200

    compiler 3.6.0

--- private/components/Link.tsx
+++ private/components/Link.tsx
@@ -15,7 +15,7 @@ class Link extends React.Component<P & Omit<JSX.IntrinsicElements['a'], keyof P>
   serverRequest: ServerRequest = new ServerRequest('http://127.0.0.1:1337');
 
   onClick: Link['props']['onClick'] = (event) => {
-    this.serverRequest.get('/statistics.json', { parameters: { url: this.props.to, }, });
+    this.serverRequest.get('/statistics/statistics.json', { parameters: { url: this.props.to, }, });
 
     if (this.props.onClick) {
       this.props.onClick(event);
