fdd5e1bc3d4fdc1c31efb3f0a4ea5e9e386f3d0a
Marek Kobida <marek.kobida@gmail.com>
2020-06-28 22:14:16 +0200

    updated Text component (skúška)

--- private/components/Text.tsx
+++ private/components/Text.tsx
@@ -4,15 +4,48 @@ import createResponsiveClassName, { ResponsiveClassName, } from '../helpers/crea
 interface P {
   alignment?: ResponsiveClassName<'#' | 'l' | 'r'>;
   color?: string;
-  m?: ResponsiveClassName<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
-  p?: ResponsiveClassName<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  m?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  mB?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  mL?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  mR?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  mT?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  mX?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  mY?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  p?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  pB?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  pL?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  pR?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  pT?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  pX?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
+  pY?: ResponsiveClassName<'!0' | '!1' | '!2' | '!3' | '!4' | '!5' |'!6' |'!7' |'!8' |'!16' |'#' | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 16>;
   size?: 1 | 2 | 3 | 4 | 5 | 6;
   weight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
 }
 
 class Text extends React.Component<P & Omit<JSX.IntrinsicElements['p'], keyof P>> {
   render () {
-    const { alignment, className, color, m, p, size, weight, ...props } = this.props;
+    const {
+      alignment,
+      className,
+      color,
+      m,
+      mB,
+      mL,
+      mR,
+      mT,
+      mX,
+      mY,
+      p,
+      pB,
+      pL,
+      pR,
+      pT,
+      pX,
+      pY,
+      size,
+      weight,
+      ...props
+    } = this.props;
 
     return (
       <p
@@ -21,7 +54,19 @@ class Text extends React.Component<P & Omit<JSX.IntrinsicElements['p'], keyof P>
           color && `color_${color}`,
           size && `h${size}`,
           createResponsiveClassName('m-', m),
+          createResponsiveClassName('m-b-', mB),
+          createResponsiveClassName('m-l-', mL),
+          createResponsiveClassName('m-r-', mR),
+          createResponsiveClassName('m-t-', mT),
+          createResponsiveClassName('m-x-', mX),
+          createResponsiveClassName('m-y-', mY),
           createResponsiveClassName('p-', p),
+          createResponsiveClassName('p-b-', pB),
+          createResponsiveClassName('p-l-', pL),
+          createResponsiveClassName('p-r-', pR),
+          createResponsiveClassName('p-t-', pT),
+          createResponsiveClassName('p-x-', pX),
+          createResponsiveClassName('p-y-', pY),
           createResponsiveClassName('t_alignment_', alignment),
           weight && `t_weight_${weight}`,
           className,
