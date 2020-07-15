d27096cca7515527c7b2a4d1422e47fba384364d
Marek Kobida <marek.kobida@gmail.com>
2020-07-15 15:03:41 +0200

    updated babel-plugin-decodeClassName.js

--- private/components/babel-plugin-decodeClassName.js
+++ private/components/babel-plugin-decodeClassName.js
@@ -37,7 +37,7 @@ module.exports = ({ types: t, }) => {
         },
         exit (path, state) {
           if (state.$) {
-            path.node.body.unshift(t.importDeclaration([ t.importDefaultSpecifier(state.decodeClassNameIdentifier), ], t.stringLiteral('@redredsk/helpers/private/decodeClassName')));
+            path.node.body.unshift(t.importDeclaration([ t.importDefaultSpecifier(state.decodeClassNameIdentifier), ], t.stringLiteral('@redredsk/design/private/components/decodeClassName')));
           }
         },
       },
