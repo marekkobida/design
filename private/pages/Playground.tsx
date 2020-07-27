45817b2f7bcabb1724806a309cbe2a3d60e909f3
Marek Kobida <marek.kobida@gmail.com>
2020-07-27 16:27:41 +0200

    updated design

--- private/pages/Playground.tsx
+++ private/pages/Playground.tsx
@@ -16,8 +16,6 @@ import Row from '../components/Row';
 import css from './Playground.css';
 import Test from './components/Test';
 
-console.log(css);
-
 class Playground extends Page {
   constructor () {
     super(-1, 'playground');
@@ -25,9 +23,37 @@ class Playground extends Page {
 
   element () {
     return (
-      <Container>
-        <Test mY={8} />
-        <Div mY={8}>
+      <>
+        <Div className={css.test} pX={3}>
+          <Row alignItems="center">
+            <Column columnSize="width" mL="#">
+              <Row alignItems="center">
+                <Column>
+                  <Anchor href="#" pY={2}>Anchor</Anchor>
+                </Column>
+                <Column>
+                  <Anchor href="#" pY={2}>Anchor</Anchor>
+                </Column>
+                <Column>
+                  <Button type="button">Button</Button>
+                </Column>
+              </Row>
+            </Column>
+          </Row>
+          <Row alignItems="center">
+            <Column columnSize="width">
+              <Anchor className="border-bottom display-block" href="#" pY={4}>Anchor</Anchor>
+            </Column>
+            <Column columnSize="width">
+              <Anchor className="display-block" href="#" pY={4}>Anchor</Anchor>
+            </Column>
+            <Column columnSize="width">
+              <Anchor className="display-block" href="#" pY={4}>Anchor</Anchor>
+            </Column>
+          </Row>
+        </Div>
+        <Container>
+          <Test />
           <Anchor href="#">Anchor</Anchor>
           <Heading size={1}>Heading 1</Heading>
           <Heading size={2}>Heading 2</Heading>
@@ -36,33 +62,33 @@ class Playground extends Page {
           <Heading size={5}>Heading 5</Heading>
           <Heading size={6}>Heading 6</Heading>
           <Paragraph>Paragraph</Paragraph>
-        </Div>
-        <Row mY={8}>
-          <Column columnSize={[ 12, { '#': 6, }, ]}>
-            <Div mY={2}>
-              <Label htmlFor="a" mB={2}>Label</Label>
-              <Input id="a" placeholder="Input" type="text" />
-            </Div>
-            <Div mY={2}>
-              <Label htmlFor="b" mB={2}>Label</Label>
-              <Input id="b" type="radio" />
-            </Div>
-            <Div mY={2}>
-              <Label htmlFor="c" mB={2}>Label</Label>
-              <Input id="c" type="checkbox" />
-            </Div>
-          </Column>
-          <Column columnSize={[ 12, { '#': 6, }, ]}>
-            <Div mY={2}>
-              <Label htmlFor="d" mB={2}>Label</Label>
-              <Input id="d" placeholder="Input" type="text" />
-            </Div>
-            <Div mY={2}>
-              <Button type="button">Button</Button>
-            </Div>
-          </Column>
-        </Row>
-      </Container>
+          <Row className="border">
+            <Column className="border" columnSize={[ 12, { '#': 6, }, ]}>
+              <Div mY={2}>
+                <Label htmlFor="a" mB={2}>Label</Label>
+                <Input id="a" placeholder="Input" type="text" />
+              </Div>
+              <Div mY={2}>
+                <Label htmlFor="b" mB={2}>Label</Label>
+                <Input id="b" type="radio" />
+              </Div>
+              <Div mY={2}>
+                <Label htmlFor="c" mB={2}>Label</Label>
+                <Input id="c" type="checkbox" />
+              </Div>
+            </Column>
+            <Column className="border" columnSize={[ 12, { '#': 6, }, ]}>
+              <Div mY={2}>
+                <Label htmlFor="d" mB={2}>Label</Label>
+                <Input id="d" placeholder="Input" type="text" />
+              </Div>
+              <Div mY={2}>
+                <Button type="button">Button</Button>
+              </Div>
+            </Column>
+          </Row>
+        </Container>
+      </>
     );
   }
 
--- private/pages/components/Test.tsx
+++ private/pages/components/Test.tsx
@@ -23,7 +23,6 @@ const properties = test({
 
 interface S {
   properties: { [propertyName in keyof typeof properties]: typeof properties[propertyName][number] };
-  t: boolean;
 }
 
 class Test extends React.Component<React.ComponentPropsWithoutRef<'div'> & CommonParameters, S> {
@@ -36,60 +35,48 @@ class Test extends React.Component<React.ComponentPropsWithoutRef<'div'> & Commo
       justifyContent: 'space-between',
       textAlign: 'left',
     },
-    t: false,
   };
 
   render () {
     let $: React.ReactNode[] = [];
 
-    if (this.state.t) {
-      for (const propertyName in properties) {
-        $ = [
-          ...$,
-          (
-            <Column columnSize={6} key={propertyName}>
-              <Heading mY={2} size={6}>{propertyName}</Heading>
-              {
-                properties[propertyName].map((property) => (
-                  <Div alignItems="center" className="display-inline-flex" key={property} p={2}>
-                    <Input
-                      defaultChecked={property === this.state.properties[propertyName]}
-                      id={`${propertyName}-${property}`}
-                      mR={2}
-                      name={propertyName}
-                      onClick={() => this.setState((state) => ({ ...state, properties: { ...state.properties, [propertyName]: property, }, }))}
-                      type="radio"
-                      value={property}
-                    />
-                    <Label htmlFor={`${propertyName}-${property}`}>{property}</Label>
-                  </Div>
-                ))
-              }
-            </Column>
-          ),
-        ];
-      }
+    for (const propertyName in properties) {
+      $ = [
+        ...$,
+        (
+          <Column className="border" columnSize={6} key={propertyName}>
+            <Heading mY={2} size={6}>{propertyName}</Heading>
+            {
+              properties[propertyName].map((property) => (
+                <Div alignItems="center" className="display-inline-flex" key={property} p={2}>
+                  <Input
+                    defaultChecked={property === this.state.properties[propertyName]}
+                    id={`${propertyName}-${property}`}
+                    mR={2}
+                    name={propertyName}
+                    onClick={() => this.setState((state) => ({ ...state, properties: { ...state.properties, [propertyName]: property, }, }))}
+                    type="radio"
+                    value={property}
+                  />
+                  <Label htmlFor={`${propertyName}-${property}`}>{property}</Label>
+                </Div>
+              ))
+            }
+          </Column>
+        ),
+      ];
     }
 
     return (
       <Div {...this.props}>
-        <Div alignItems="center" className="display-flex" mY={2}>
-          <Input
-            id="t"
-            mR={2}
-            onClick={() => this.setState((state) => ({ ...state, t: !this.state.t, }))}
-            type="checkbox"
-          />
-          <Label htmlFor="t">Som vývojár</Label>
-        </Div>
-        {this.state.t && <Row>{$}</Row>}
+        <Row className="border">{$}</Row>
         <Div style={{ paddingBottom: '50%', position: 'relative', }}>
-          <Row {...this.state.properties} style={{ bottom: 0, left: 0, position: 'absolute', right: 0, top: 0, }}>
-            <Column columnSize="#">#</Column>
-            <Column columnSize="width">width</Column>
-            <Column columnSize={12}>Šírka 12. stĺpcov</Column>
-            <Column columnSize={4}>Šírka 4. stĺpcov</Column>
-            <Column columnSize={4}>Šírka 4. stĺpcov</Column>
+          <Row {...this.state.properties} className="border" style={{ bottom: 0, left: 0, position: 'absolute', right: 0, top: 0, }}>
+            <Column className="border" columnSize="#">"#"</Column>
+            <Column className="border" columnSize="width">"width"</Column>
+            <Column className="border" columnSize={12}>12</Column>
+            <Column className="border" columnSize={4}>4</Column>
+            <Column className="border" columnSize={4}>4</Column>
           </Row>
         </Div>
       </Div>
