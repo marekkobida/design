16fed8b93f66755b32cdf323520c3256e40898a7
Marek Kobida <marek.kobida@gmail.com>
2020-07-27 15:00:09 +0200

    updated design

--- private/css/components/Button.ts
+++ private/css/components/Button.ts
@@ -3,22 +3,25 @@ import CSS from '../CSS';
 class Button extends CSS {
   css (): string {
     return `button {
-  --button\\(background-color\\): 255, 255, 255;
+  --button\\(background-color\\): var(--color);
   --button\\(border-color\\): var(--color);
-  appearance: button;
-  background-color: rgb(var(--button\\(background-color\\)));
-  border: 0.125rem solid rgba(var(--button\\(border-color\\)), 0.25);
-  border-radius: 0.25rem;
+  --button\\(color\\): 255, 255, 255;
+  -webkit-appearance: button;
+  background-color: rgba(var(--color), 0.5);
+  border: 0;
+  border-radius: 0.1875rem;
+  color: rgb(var(--button\\(color\\)));
   display: inline-block;
   font-family: inherit;
   font-size: inherit;
   line-height: inherit;
   margin: 0;
   padding: 0.75rem 1.25rem;
+  transition: background-color 0.125s ease-in-out;
 }
 button:focus,
 button:hover {
-  border-color: rgba(var(--button\\(border-color\\)), 0.75);
+  background-color: rgba(var(--color), 0.75);
   outline: 0;
 }
 button:not(:disabled) {
--- private/css/components/Input.ts
+++ private/css/components/Input.ts
@@ -5,43 +5,39 @@ class Input extends CSS {
     return `input {
   --input\\(background-color\\): 255, 255, 255;
   --input\\(border-color\\): var(--color);
-  appearance: none;
+  -webkit-appearance: none;
+  background-color: rgb(var(--input\\(background-color\\)));
+  border: 0.125rem solid rgba(var(--input\\(border-color\\)), 0.5);
+  border-radius: 0.1875rem;
   display: block;
   font-family: inherit;
   font-size: inherit;
   font-weight: inherit;
   line-height: inherit;
   margin: 0;
+  transition: border-color 0.125s ease-in-out;
 }
-input[type="checkbox"] {
-  background-color: rgb(var(--input\\(background-color\\)));
-  border: 0.125rem solid rgba(var(--input\\(border-color\\)), 0.25);
-  border-radius: 0.25rem;
-  height: 1.25rem;
-  width: 1.25rem;
+input::placeholder {
+  color: rgba(var(--input\\(border-color\\)), 0.25);
 }
-input[type="checkbox"]:checked {
-  border-color: rgba(var(--color), 0.75);
-  border-width: 0.25rem;
-}
+input[type="checkbox"],
 input[type="radio"] {
-  background-color: rgb(var(--input\\(background-color\\)));
-  border: 0.125rem solid rgba(var(--input\\(border-color\\)), 0.25);
-  border-radius: 50%;
   height: 1.25rem;
   width: 1.25rem;
 }
+input[type="checkbox"]:checked,
 input[type="radio"]:checked {
   border-color: rgba(var(--color), 0.75);
-  border-width: 0.25rem;
+  border-width: 0.1875rem;
 }
+input[type="checkbox"]:focus,
 input[type="radio"]:focus {
   outline: 0;
 }
+input[type="radio"] {
+  border-radius: 50%;
+}
 input[type="text"] {
-  background-color: rgb(var(--input\\(background-color\\)));
-  border: 0.125rem solid rgba(var(--input\\(border-color\\)), 0.25);
-  border-radius: 0.25rem;
   padding: 0.75rem 1.25rem;
   width: 100%;
 }
