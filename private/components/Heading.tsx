3644e8add2ffb3555804bb517ce483ffebe83e15
Marek Kobida <marek.kobida@gmail.com>
2020-07-28 14:05:33 +0200

    updated Heading component (size -> headingSize)

--- private/components/Heading.tsx
+++ private/components/Heading.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import decodeClassName from '../helpers/decodeClassName';
 import decodeCommonParameters, { CommonParameters, } from '../helpers/decodeCommonParameters';
 
-interface P { size: 1 | 2 | 3 | 4 | 5 | 6; }
+interface P { headingSize: 1 | 2 | 3 | 4 | 5 | 6; }
 
 class Heading extends React.Component<React.ComponentPropsWithoutRef<'h1'> & P & CommonParameters> {
   render () {
-    const { className, size, ...notCommonParameters } = decodeCommonParameters(this.props);
+    const { className, headingSize, ...notCommonParameters } = decodeCommonParameters(this.props);
 
-    const H = `h${size}` as 'h1';
+    const H = `h${headingSize}` as 'h1';
 
     return <H {...notCommonParameters} className={decodeClassName(className)} />;
   }
