ddc5f2ea4689e67722114167d1ecebbb57ed84ca
Marek Kobida <marek.kobida@gmail.com>
2020-08-05 21:29:38 +0200

    updated design

--- private/pages/index.ts
+++ private/pages/index.ts
@@ -1,16 +1,8 @@
 import Container from '@redredsk/pages/private/Container';
 
 import Playground from './Playground';
-import SignUp from './SignUp';
 
-const container = new Container(
-  -1,
-  'design',
-  [
-    new Playground(),
-    new SignUp(),
-  ]
-);
+const container = new Container(-1, 'design', [ new Playground(), ]);
 
 container.toDOM();
 
