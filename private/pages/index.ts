2213f885d33dae4f4c11b6500c7aac29e6cd16c4
Marek Kobida <marek.kobida@gmail.com>
2020-08-12 11:26:43 +0200

    deleted pages from design repo.

--- private/pages/index.ts
+++
@@ -1,9 +0,0 @@
-import Container from '@redredsk/pages/private/Container';
-
-import Playground from './Playground';
-
-const container = new Container(-1, 'design', [ new Playground(), ]);
-
-container.toDOM();
-
-export default container;
