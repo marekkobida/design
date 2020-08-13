2ea8215383340948cdbcca538d7b010b19d8baf6
Marek Kobida <marek.kobida@gmail.com>
2020-08-13 13:11:49 +0200

    removed server stuff from Anchor component

--- private/htmlComponents/Anchor.tsx
+++ private/htmlComponents/Anchor.tsx
@@ -1,26 +1,13 @@
-import ServerRequest from '@redredsk/helpers/private/ServerRequest';
 import React from 'react';
 
 import decodeClassName from '../helpers/decodeClassName';
 import decodeCommonParameters, { CommonParameters, } from '../helpers/decodeCommonParameters';
 
 class Anchor extends React.Component<CommonParameters & Omit<React.ComponentPropsWithoutRef<'a'>, keyof CommonParameters>> {
-  serverRequest: ServerRequest = new ServerRequest('http://127.0.0.1:1337');
-
-  onClick: Anchor['props']['onClick'] = (event) => {
-    if (this.props.href) {
-      this.serverRequest.get('/statistics/statistics.json', { parameters: { url: this.props.href, }, });
-    }
-
-    if (this.props.onClick) {
-      this.props.onClick(event);
-    }
-  }
-
   render () {
     const { className, ...notCommonParameters } = decodeCommonParameters(this.props);
 
-    return <a {...notCommonParameters} className={decodeClassName(className)} onClick={this.onClick} />;
+    return <a {...notCommonParameters} className={decodeClassName(className)} />;
   }
 }
 
