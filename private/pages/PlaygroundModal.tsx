8f8c0960f4195f76d7ef7db30f44a265be7a73fc
Peter Mikus <peminator@gmail.com>
2020-08-13 14:34:07 +0200

    add tab key preventer only if open modal

--- private/pages/PlaygroundModal.tsx
+++ private/pages/PlaygroundModal.tsx
@@ -1,9 +1,6 @@
 import Container from '../components/Container';
-import Heading from '../components/Heading';
-import Link from '../components/Link';
 import Page from '@redredsk/pages/private/Page';
 import React, { FocusEventHandler, useState, } from 'react';
-import Text from '../components/Text';
 import template from '@redredsk/pages/private/template';
 
 // MODAL IMPORTS
@@ -12,16 +9,17 @@ import Modal from '../components/modal/Modal';
 
 class PlaygroundModal extends Page {
   constructor () {
-    super(-2, 'PlaygroundModal');
+    super(-2, 'Modal');
   }
 
 
   modaltest1 () {
     const { isShowing, toggleModal,  } = useModal();
+    // avoid tab navigate away, if modal is open
     return (
       <div
-        onKeyUp={(e)=>{ if (e.which == 9) { e.preventDefault(); } }}
-        onKeyDown={(e)=>{ if (e.which == 9) { e.preventDefault(); } }}
+        onKeyUp={(e)=>{ if (isShowing === true && e.which == 9) { e.preventDefault(); } }}
+        onKeyDown={(e)=>{ if (isShowing === true && e.which == 9) { e.preventDefault(); } }}
       >
         <button className="button-default" onClick={toggleModal}>Show Modal</button>
         <Modal
@@ -37,9 +35,8 @@ class PlaygroundModal extends Page {
     const { isShowing, toggleModal, } = useModal();
     return (
       <div
-        onKeyUp={(e)=>{ if (e.which == 9) { e.preventDefault(); } }}
-        onKeyDown={(e)=>{ if (e.which == 9) { e.preventDefault(); } }}
-
+        onKeyUp={(e)=>{ if (isShowing === true && e.which == 9) { e.preventDefault(); } }}
+        onKeyDown={(e)=>{ if (isShowing === true && e.which == 9) { e.preventDefault(); } }}
       >
         <button className="button-default" onClick={toggleModal}>Show Modal</button>
         <Modal
@@ -55,17 +52,10 @@ class PlaygroundModal extends Page {
   element () {
     return (
       <Container>
-        <Heading size={1}>{this.name}</Heading>
-        <Heading size={2}>{this.name}</Heading>
-        <Heading size={3}>{this.name}</Heading>
-        <Heading size={4}>{this.name}</Heading>
-        <Heading size={5}>{this.name}</Heading>
-        <Heading size={6}>{this.name}</Heading>
+        Huh ?
 
-        <Link to="#">{this.name}</Link>
-        <Text>{this.name}</Text>
         <this.modaltest1 />
-       Lorep Gipsum
+       Lorep Gipsumsddsd
         <this.modaltest2 />
       </Container>
     );
--- private/pages/index.ts
+++ private/pages/index.ts
@@ -1,8 +1,14 @@
 import Container from '@redredsk/pages/private/Container';
 
 import Playground from './Playground';
+import PlaygroundModal from './PlaygroundModal';
 
-const container = new Container(-1, 'design', [ new Playground(), ]);
+const container = new Container(-1, 'design', [
+
+  new Playground(),
+  new PlaygroundModal(),
+
+]);
 
 container.toDOM();
 
