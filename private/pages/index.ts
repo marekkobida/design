471ebd59be3827822d084a2ae9d3fdd3c97be812
Marek Kobida <marek.kobida@gmail.com>
2020-08-17 10:39:29 +0200

    deleted modal from design (backup, záloha) - úplne všetko čo sa dalo je zle... evidentne vypnutý typescript... vypnutý LINTER ty kks... celé napísané zle a nie podľa zadania...

--- private/pages/index.ts
+++ private/pages/index.ts
@@ -1,14 +1,8 @@
 import Container from '@redredsk/pages/private/Container';
 
 import Playground from './Playground';
-import PlaygroundModal from './PlaygroundModal';
 
-const container = new Container(-1, 'design', [
-
-  new Playground(),
-  new PlaygroundModal(),
-
-]);
+const container = new Container(-1, 'design', [ new Playground(), ]);
 
 container.toDOM();
 
