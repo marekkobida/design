b1e9af588367fe30f746a89c9793139de63447a1
Marek Kobida <marek.kobida@gmail.com>
2020-08-18 13:57:45 +0200

    updated design "width" commonParameter (záloha)

--- private/components/Column.tsx
+++ private/components/Column.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Div from './html/Div';
 
 class Column extends React.Component<Div['props']> {
-  static defaultProps = { pX: 2, };
+  static defaultProps = { pX: 2, width: 'auto', };
 
   render () {
     return <Div {...this.props} />;
--- private/css/commonParameters/width.ts
+++ private/css/commonParameters/width.ts
@@ -6,6 +6,7 @@ function width (): string {
     (breakpoint) => {
       const $ = f(
         ($, i) => `.${breakpoint.left}width-${i + 1}\\/12 {
+  flex: 0 0 auto !important;
   width: ${(i + 1) / 12 * 100}% !important;
 }`,
         11
@@ -13,12 +14,15 @@ function width (): string {
 
       return `${$}
 .${breakpoint.left}width-0 {
+  flex: 0 0 auto !important;
   width: 0 !important;
 }
 .${breakpoint.left}width-100 {
+  flex: 0 0 auto !important;
   width: 100% !important;
 }
 .${breakpoint.left}width-auto {
+  flex: 0 0 auto !important;
   width: auto !important;
 }`;
     }
