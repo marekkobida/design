40ecad0ee3bb3b0e9849f35f720ca7603d4f70c1
Marek Kobida <marek.kobida@gmail.com>
2020-10-11 23:55:40 +0200

    upgraded design

---
+++ private/helpers/common.types.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2020 Marek Kobida
+ */
+
+import React from 'react';
+
+import { CommonParameters, } from './decodeCommonParameters';
+
+export type Test<T extends React.ElementType> = CommonParameters & Omit<React.ComponentPropsWithoutRef<T>, keyof CommonParameters>;
